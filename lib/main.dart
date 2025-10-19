09f3b361db18cd9084fc62ad0ded1d909dd36b13
Kseniia <email@ks.ks.krom@gmail.com>
2025-10-20 02:24:37 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:idea_list_flutter/screens/home_screen.dart';
+import 'helper/shared_pref.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SharedPrefsHelper.init();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(),
+      home: IdeasScreen(),
+    );
+  }
+}
